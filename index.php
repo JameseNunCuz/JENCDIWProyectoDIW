43bd365293c6e9b69f84d9582254da3da17fd420
JameseNunCuz <jamese.nuncuz.1@educa.jcyl.es>
2025-10-24 09:40:39 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James Edward Nuñez Cuzcano</title>
+    <link rel="stylesheet" href="./webroot/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Desarrollo de interfaces web</h1>
+        </div>
+        <button class="home active" onclick="location.href = '../'"><img src="./webresources/home.png"></button>
+    </div>
+
+    <div class="center-container">
+        <button onclick="location.href = './webroot/ejercicios/ejercicio1.html'" class="active"><p>Practica 1</p></button>
+        <button onclick="location.href = './webroot/ejercicios/ejercicio2.html'" class="active"><p>Practica 1</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr1-1-1.html'"  class="active"><p>Ejercicio 1-1-1</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr1-1-2.html'" class="active"><p>Ejercicio 1-1-2</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr1-2-1.html'" class="active"><p>Ejercicio 1-2-1</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr1-2-3.html'" class="active"><p>Ejercicio 1-2-3</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr2-1.html'" class="active"><p>Ejercicio 2-1</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr2-3.html'" class="active"><p>Ejercicio 2-3</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr2-4.html'" class="active"><p>Ejercicio 2-4</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr2-5.html'" class="active"><p>Ejercicio 2-5</p></button>
+        <button onclick="location.href = './webroot/ejercicios/pr2-6.html'" class="active"><p>Ejercicio 2-6</p></button>
+    </div>
+
+    <div class="footer">
+        <h3>James Edward Nuñez Cuzcano</h3>
+    </div>
+</body>
+
+</html>
